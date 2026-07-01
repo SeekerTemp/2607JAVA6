e28ae6ab293fbf2f2a42c3d452faf4ffa7ab890f
SeekerTemp <gm.bac.3d@gmail.com>
2026-07-01 13:21:02 +0700

    lab 1 submit

--- demob1/src/main/java/com/vn/test/demob1/LAB/security/SecurityConfig.java
+++ demob1/src/main/java/com/vn/test/demob1/LAB/security/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
+//Config lab 1
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
